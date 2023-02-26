8d90e09837ca683c459af15c30f2625883a3de51
tomcyrus <tomcyrus0@gmail.com>
2023-02-26 14:12:10 +0100

    extra

--- src/pages/extracoop.js
+++ src/pages/extracoop.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { ThemeProvider } from 'theme-ui';
-// import theme from 'theme';
-// import SEO from 'components/seo';
-// import Layout from 'components/layout';
-// import Extra from 'sections/extra';
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Extra from 'sections/extra';
 
-// export default function IndexPage() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Layout>
-//         <SEO
-//           title="Extramile Africa"
-//           description="Extramile Africa is your hybrid cooperative plugin to access everyday products and services on credit."
-//         />
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO
+          title="Extramile Africa"
+          description="Extramile Africa is your hybrid cooperative plugin to access everyday products and services on credit."
+        />
         
-//         <Extra/>
+        <Extra/>
        
-//       </Layout>
-//     </ThemeProvider>
-//   );
-// }
+      </Layout>
+    </ThemeProvider>
+  );
+}
